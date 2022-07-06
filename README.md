3bcb32a9ea85302cea24cd72a89d0677199d8056
Kenneth <kenxx@users.noreply.github.com>
2022-07-06 19:51:16 +0800

    Feature/initial (#2)
    * initial

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# shorten
+# Shorten
+
+Shorten is a simple short url service written by golang.
+
+# Install
+
+```shell
+# setup listen host
+export SHORTEN_HOST="<listen host>" # default is empty
+
+# setup listen port
+export SHORTEN_PORT="<listen port>" # default is 8080
+
+# setup short url path
+export SHORTEN_BASE_PATH="<shorten base path>" # default is empty string
+## e.g. set to "/test/" then your short url is "https://<host>/test/<auto_key>"
+
+# setup postgres connection string
+export SHORTEN_POSTGRES="<postgres connection string>" # default is root@localhost
+## default is "host=localhost port=5432 user=root password=root database=root sslmode=disable"
+
+# install pkg
+go mod tidy
+
+# build
+go build cmd/shorten/shorten.go
+
+# run
+./shorten
+```
+
+# Todo
+
+- [ ] Memory Cache
+- [ ] Redis Cache
---
+++ cmd/shorten/shorten.go
@@ -0,0 +1,254 @@
+package main
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"math/bits"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/lib/pq"
+)
+
+const TableName = "urls"
+
+const TableExists = `SELECT exists(
+	SELECT FROM information_schema.tables 
+		WHERE table_schema = 'public' AND table_type LIKE 'BASE TABLE' AND table_name = '` + TableName + `'
+);`
+
+const CreateTable = `CREATE TABLE ` + TableName + `
+(
+    id         serial PRIMARY KEY,
+    auto_key   character varying(64) UNIQUE,
+    custom_key character varying(64) UNIQUE,
+    url        character varying(4096) NOT NULL CHECK ( LENGTH(url) > 0 ) UNIQUE,
+    status     integer                 NOT NULL DEFAULT 0,
+    expired_at timestamp,
+    created_at timestamp               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (auto_key, url),
+    UNIQUE (custom_key, url)
+);`
+
+var SupportedPrefix = []string{"https://", "http://"}
+
+var DB *Database
+
+type Database struct {
+	Migrated   bool
+	Connection *sql.DB
+}
+
+func NewDatabase(connStr string) (*Database, error) {
+	db := new(Database)
+	err := db.Init(connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (db *Database) Init(connStr string) error {
+	conn, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return err
+	}
+
+	db.Connection = conn
+
+	var exists bool
+	if err = db.Connection.QueryRow(TableExists).Scan(&exists); err != nil {
+		log.Error(err)
+		return err
+	}
+	if !exists {
+		if _, err = db.Connection.Query(CreateTable); err != nil {
+			log.Error(err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (db *Database) AddUrl(url string, customKey string) (string, error) {
+	url = strings.Trim(url, "\t\n ")
+	supported := false
+	for _, prefix := range SupportedPrefix {
+		if strings.HasPrefix(url, prefix) {
+			supported = true
+			break
+		}
+	}
+
+	if !supported {
+		return "", errors.New("url not supported")
+	}
+
+	var id uint64
+	if err := db.Connection.
+		QueryRow("SELECT id FROM "+TableName+" WHERE url = $1 LIMIT 1;", url).
+		Scan(&id); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			break
+		default:
+			return "", err
+		}
+	}
+
+	if id == 0 {
+		if err := db.Connection.
+			QueryRow("INSERT INTO "+TableName+" (url) VALUES ($1) RETURNING id;", url).
+			Scan(&id); err != nil {
+			return "", err
+		}
+	}
+
+	sum := sha256.Sum256([]byte(url))
+	sumKey := uint64(sum[1]) + (uint64(sum[0]) << 8)
+	baseFix := 64 - bits.LeadingZeros64(id)
+	urlKey := strconv.FormatUint(id|sumKey<<baseFix, 36)
+
+	if customKey == "" {
+		customKey = urlKey
+	}
+
+	if _, err := db.Connection.
+		Query("UPDATE "+TableName+" SET auto_key = $1, custom_key = $2 WHERE id = $3", urlKey, customKey, id); err != nil {
+		return "", err
+	}
+
+	return urlKey, nil
+}
+
+func (db *Database) FindByKey(key string) (string, bool) {
+	var url string
+	err := db.Connection.QueryRow("SELECT url FROM "+TableName+" WHERE auto_key = $1 OR custom_key = $1 ORDER BY id DESC LIMIT 1", key).Scan(&url)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return "", false
+		default:
+			log.Error(err)
+			return "", false
+		}
+	}
+
+	return url, true
+}
+
+func redirect(c echo.Context) error {
+	key := c.Param("key")
+
+	url, ok := DB.FindByKey(key)
+	if !ok {
+		log.Error("find key(" + key + ") error")
+		url = "/"
+	}
+
+	return c.Redirect(http.StatusMovedPermanently, url)
+}
+
+type Result struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type AddURLRequest struct {
+	URL       string `json:"url" validate:"required"`
+	CustomKey string `json:"custom_key"`
+}
+
+func addUrl(c echo.Context) error {
+	url := new(AddURLRequest)
+	err := c.Bind(url)
+	if err != nil {
+		return err
+	}
+	var urlKey string
+	if urlKey, err = DB.AddUrl(url.URL, url.CustomKey); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, &Result{
+		Code:    0,
+		Message: "",
+		Data: map[string]string{
+			"uri": ShortenBasePath + urlKey,
+		},
+	})
+}
+
+var ShortenHost = ""
+var ShortenPort = 8080
+var ShortenBasePath = "/"
+var ShortenPostgresConnectionString = "host=localhost port=5432 user=root password=root database=root sslmode=disable"
+
+func init() {
+	if v := os.Getenv("SHORTEN_HOST"); v != "" {
+		ShortenHost = v
+		log.Info("host is set to " + ShortenHost)
+	}
+	if v := os.Getenv("SHORTEN_PORT"); v != "" {
+		p, err := strconv.Atoi(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ShortenPort = p
+		log.Info(fmt.Sprintf("host is set to %d", ShortenPort))
+	}
+	if v := os.Getenv("SHORTEN_BASE_PATH"); v != "" {
+		ShortenBasePath = "/" + strings.Trim(v, "/") + "/"
+		log.Info("base path is set to " + ShortenBasePath)
+	}
+	if v := os.Getenv("SHORTEN_POSTGRES"); v != "" {
+		ShortenPostgresConnectionString = v
+		log.Info("postgres is set")
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	if DB == nil {
+		var err error
+		if DB, err = NewDatabase(ShortenPostgresConnectionString); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	e.HTTPErrorHandler = func(err error, c echo.Context) {
+		code := http.StatusInternalServerError
+		if he, ok := err.(*echo.HTTPError); ok {
+			code = he.Code
+		}
+		c.Logger().Error(err)
+		if err = c.JSON(code, Result{
+			Code:    code,
+			Message: "System Error: " + err.Error(),
+		}); err != nil {
+			c.Logger().Error(err)
+		}
+	}
+
+	e.Static("/", "public")
+	e.File("/favicon.ico", "public/favicon.ico")
+	e.GET(ShortenBasePath+":key", redirect)
+	e.POST("/api/add-url", addUrl)
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%d", ShortenHost, ShortenPort)))
+}
